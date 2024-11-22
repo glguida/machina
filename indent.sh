7efc90c2fd1b50be4e165629365a2393d29dde21
Gianluca Guida <glguida@tlbflush.org>
2024-11-22 19:36:31 +0000

    add useful scripts

---
+++ indent.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+
+STYLE=-nbad -bap -nbc -bbo -bl -bli2 -bls -ncdb -nce -cp1 -cs -di2 \
+-ndj -nfc1 -nfca -hnl -i2 -ip5 -lp -pcs -nprs -psl -saf -sai \
+-saw -nsc -nsob
+
+for i in `find . | grep \\\.[ch]$ | grep -v contrib/ | grep -v tools/mig`; do echo $i; indent ${STYLE} $i; done
