b03c59b31c676b6666d32b1b8091c39a17413fc9
Gianluca Guida <glguida@tlbflush.org>
2024-12-05 05:57:47 +0000

    indent.sh: do not indent libmach3

--- indent.sh
+++ indent.sh
@@ -6,4 +6,4 @@ STYLE=-nbad -bap -nbc -bbo -bl -bli2 -bls -ncdb -nce -cp1 -cs -di2 \
 -ndj -nfc1 -nfca -hnl -i2 -ip5 -lp -pcs -nprs -psl -saf -sai \
 -saw -nsc -nsob
 
-for i in `find . | grep \\\.[ch]$ | grep -v contrib/ | grep -v tools/mig`; do echo $i; indent ${STYLE} $i; done
+for i in `find . | grep \\\.[ch]$ | grep -v contrib/ | grep -v tools/mig | grep -v libmach3/`; do echo $i; indent ${STYLE} $i; done
